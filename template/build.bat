dd885ceae3fb05fc577e61b225661078c8ac7852
CCC200 <megamanzero94@hotmail.com>
2024-06-12 00:01:08 -0400

    Include template folder

---
+++ template/build.bat
@@ -0,0 +1,37 @@
+@echo off
+title template
+
+set args=%*
+if "%args%" == "" set args=release
+call :checkcmd clean
+call :checkcmd refresh
+call :checkcmd debug
+call :checkcmd release
+exit /b 0
+
+:release
+call bytepack -pack assets
+if %errorlevel% equ 0 call haxelib run openfl build windows -Dno-deprecation-warnings %BYTEPACK_FLAGS%
+exit /b 0
+
+:debug
+call bytepack -clean assets
+call haxelib run openfl test windows -Dno-deprecation-warnings -debug
+exit /b 0
+
+:clean
+echo | set /p=Cleaning...
+rmdir /s /q build
+echo OK
+exit /b 1
+
+:refresh
+echo | set /p=Cleaning...
+rmdir /s /q build\windows\bin
+echo OK
+exit /b 1
+
+:checkcmd
+echo.%args% | findstr /i /c:"%~1" 1>nul
+if %errorlevel% equ 0 call :%~1
+exit /b %errorlevel%
---
+++ template/src/Main.hx
@@ -0,0 +1,16 @@
+package;
+
+import openfl.display.Sprite;
+
+import com.zeroip.BytePack;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		BytePack.init();
+		
+		
+	}
+}
